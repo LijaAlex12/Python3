1f6bae7962c730eee8e7416c7ef9cf1bccfe8597
LijaAlex12 <lijaalexhere@gmail.com>
2020-03-28 22:27:23 +0530

    set set() .add(item) .remove(item) .clear .pop() len(x) in not in

---
+++ set.py
@@ -0,0 +1,33 @@
+# creating a new set
+
+x={2,3,3,4}
+print(x)
+x=set()
+# strips duplicates
+list1=[]
+x=set(list1)
+
+# set comprehension random order
+x={x for x in range(10) if x>5}
+print(x)
+
+# set operations
+x=set()
+# add item
+x.add(4)
+print(x)
+# remove item
+x.remove(4)
+print(x)
+# get length
+print(len(x))
+# membership in x
+print(4 in x)
+print(4 not in x)
+# pop random item from set
+x={31,2,3,4,5,6}
+x.pop()
+print(x)
+# delete all items from set x
+x.clear()
+print(x)
