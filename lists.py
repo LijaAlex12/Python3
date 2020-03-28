22f4423510f796d8065ae5779629644cdf25f9e4
LijaAlex12 <lijaalexhere@gmail.com>
2020-03-28 19:11:37 +0530

    insert

--- lists.py
+++ lists.py
@@ -16,9 +16,27 @@ print(x)
 del(x)
 
 
-# append
+# append an item
 y=[]
-y.append(4)
+y.append([1,'t'])
 print(y)
 
+# extend:to append a sequence to a list
+# within same list
+# similar to concatenation + of lists
+a=[1,2,3]
+b=['a','b']
+c=['u']
+a.extend(b)
+a.append(c)
+print(a)
+
+# insert :item insertion at given index
+p=[5,3,8,6]
+q=['d','k']
+p.insert(1,7)
+p.insert(3,q)
+print(p)
+
+# pop
 
