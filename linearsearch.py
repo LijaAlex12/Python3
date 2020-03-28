f32bfec4abc87b4647b1b79c8f8001b3a4cf2ea9
Lija Alex <48295613+LijaAlex12@users.noreply.github.com>
2020-03-29 04:26:55 +0530

    Create linearsearch.py

---
+++ linearsearch.py
@@ -0,0 +1,13 @@
+def search(list,n): 
+  
+    for i in range(len(list)): 
+        if list[i] == n: 
+            return True
+    return False
+    
+def search(Tuple, n): 
+  
+    for i in range(len(Tuple)): 
+        if Tuple[i] == n: 
+            return True
+    return False
